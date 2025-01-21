931625a5d00aba4743a52ec7b3711261a93a2354
node-nikhila <nikhila.p@node.gyandata.com>
2025-01-22 00:07:22 +0530

    added 3

--- test1.py
+++ test1.py
@@ -1,9 +1,11 @@
 print("Testing line1")
-num = int(input("Enter 0 or 1 or 2: "))
+num = int(input("Enter 0 or 1 or 2 or 3"))
 if num==0:
     print("ZERO")
 elif num==1:
     print("ONE")
-else:
+elif num==2:
     print("TWO")
+else:
+    print("THREE")
     
